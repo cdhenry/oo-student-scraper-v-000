8c5adc85b90084a80c77aad041f63d1a5ac018ca
Chris Henry <cdhenry@gmail.com>
2018-02-25 00:35:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
-    @profile_url = student_hash[:profile_url]
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
     @@all << self
   end
 
